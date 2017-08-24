2d934eaeea767134bafc435d74f837d31ea08aff
h181221 <h181221@stud.hib.no>
2017-08-24 11:26:15 +0200

    test på git

---
+++ mat102/ceazar.py
@@ -0,0 +1,24 @@
+def main():
+    alfabet = [26]
+
+    for a in range(97, 123):
+        alfabet.append(chr(a))
+
+    x = 24
+    c = 3
+    y = 25
+    print(alfabet[x], alfabet[c], alfabet[y])
+    for k in alfabet:
+        x += 1
+        c += 1
+        y += 1
+
+        if x > 26:
+            x = 1
+        elif y > 26:
+            y = 1
+        elif c > 26:
+            c = 1
+
+        print(alfabet[x], alfabet[c], alfabet[y])
+main()
